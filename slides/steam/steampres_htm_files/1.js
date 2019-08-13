cba8513de10dc53022d4c699fa1ade8087e1ecc3
Finn P. Petersen <finnpascalpetersen@gmail.com>
2019-08-13 16:20:27 +0200

    removed slides

--- slides/steam/steampres_htm_files/1.js
+++
@@ -1,8 +0,0 @@
-window.local_HSLineChart_3_data = [
-{
- csv: "Year,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017\u000AAverage Speed,3.6,3.8,3.9,3.9,4.5,5.8,8.7,11.1,12.5,15.3,18.7",
- style: "Internet Speed in Mbps",
- theme: "1",
- title: "Average Internet Speed in Mbps"
-}
-];
--- slides/steam/steampres_htm_files/2.js
+++
@@ -1,8 +0,0 @@
-window.local_HSColStackedChart_3_data = [
-{
- csv: "Categories, 2010,2011,2012,2013,2014,2015,2016,2017\u000APhysical, 69,65,54,46,39,31,26,21\u000ADigital, 31,35,46,54,61,69,74,79",
- style: "Sales in Percent",
- theme: "1",
- title: "Game Sales Percentage"
-}
-];
